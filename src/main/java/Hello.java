b369f2c80c265c5ba9f1551b3d9757cf56b281d2
snfry <snoo999@naver.com>
2026-03-13 15:31:58 +0900

    Added a new message

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) System.out.println(args[i]);
         System.out.println("Hello world!");
+        System.out.println("Random Message");
     }
 }
