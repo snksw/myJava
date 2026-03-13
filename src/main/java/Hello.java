a9c9b94707038e69da44a59aec7520a477e4ba73
snfry <snoo999@naver.com>
2026-03-13 15:26:07 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    // public static void main(String[] args)은 프로그램에서 가장 먼저 실행되는 함수
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) System.out.println(args[i]);
+        System.out.println("Hello world!");
+    }
+}
