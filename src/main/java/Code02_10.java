16590fdf8bc905426b8175c2121b24e79f9c0b7b
snfry <snoo999@naver.com>
2026-03-19 18:54:50 +0900

    while

--- src/main/java/Code02_10.java
+++ src/main/java/Code02_10.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Code02_10 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int num;
-        try {
-            num = s.nextInt();
-            System.out.println("User Input: " + num);
-        } catch (Exception error) {
-            System.out.println("Failed to parse input");
+        int num = 0;
+        while (true) {
+            try {
+                num += s.nextInt();
+                System.out.println("New Value: " + num);
+            } catch (Exception error) {
+                System.out.println("Failed to parse input");
+                System.out.println("Current Value: " + num);
+                break;
+            }
         }
         s.close();
     }
