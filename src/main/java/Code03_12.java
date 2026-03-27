2d68afb575039feeb58be581db0912f7cdd2b235
snfry <snoo999@naver.com>
2026-03-27 14:15:34 +0900

    Add Code03_12.java

---
+++ src/main/java/Code03_12.java
@@ -0,0 +1,8 @@
+public class Code03_12 {
+    public static void main(String[] args) {
+        int num = 99;
+        System.out.println((num > 100) && (num < 200));
+        System.out.println((num == 99) || (num == 200));
+        System.out.println(!(num == 100));
+    }
+}
