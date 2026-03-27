eaa458c856152f46b6e392f619ad474e0ac6cfc8
snfry <snoo999@naver.com>
2026-03-27 15:25:58 +0900

    ãAdd Code04_12.java

---
+++ src/main/java/Code04_12.java
@@ -0,0 +1,7 @@
+public class Code04_12 {
+    public static void main(String[] args) {
+        String s = "First Java를 공부 중입니다.";
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+    }
+}
