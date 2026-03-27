315f0a7692c645d8efbd5199335c86d3b56b861b
snfry <snoo999@naver.com>
2026-03-27 15:24:55 +0900

    ãAdd Code04_10.java

---
+++ src/main/java/Code04_10.java
@@ -0,0 +1,7 @@
+public class Code04_10 {
+    public static void main(String[] args) {
+        String str = "난생처음 자바";
+        System.out.println("문자열: " + str);
+        System.out.println("문자열 길이: " + str.length());
+    }
+}
