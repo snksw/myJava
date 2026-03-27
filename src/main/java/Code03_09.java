891b943a8b575ab6d057482d6986c2353af3c999
snfry <snoo999@naver.com>
2026-03-27 14:07:07 +0900

    Modify Code03_09.java

--- src/main/java/Code03_09.java
+++ src/main/java/Code03_09.java
@@ -1,8 +1,8 @@
 public class Code03_09 {
     public static void main(String[] args) {
         int num = 20;
-        num++; System.out.print(num + " ");
-        num--; System.out.print(num + " ");
+        System.out.print(++num + " ");
+        System.out.print(--num + " ");
         num += 3; System.out.print(num + " ");
         num -= 3; System.out.print(num + " ");
         num *= 3; System.out.print(num + " ");
