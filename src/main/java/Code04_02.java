59f4fe1f282f9ac9b6942e280d26c6b59e224740
snfry <snoo999@naver.com>
2026-03-27 14:37:45 +0900

    Modify Code04_02.java

---
+++ src/main/java/Code04_02.java
@@ -0,0 +1,8 @@
+public class Code04_02 {
+    public static void main(String[] args) {
+        float f = 3.1415926535897932384626433832795028841971693993751058f;
+        double d = 3.1415926535897932384626433832795028841971693993751058;
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
