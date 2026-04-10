9f35d97ebd588a9cc1ecd2000bb145dcce3118b6
snfry <snoo999@naver.com>
2026-04-10 15:13:38 +0900

    Add Code06_10.java

---
+++ src/main/java/Code06_10.java
@@ -0,0 +1,9 @@
+public class Code06_10 {
+    public static void main(String[] args) {
+        short i = 0;
+        while (i < 3 /* || i > 2 */) {
+            System.out.println(i + " Java");
+            i++;
+        }
+    }
+}
