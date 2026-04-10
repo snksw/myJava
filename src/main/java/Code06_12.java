069bf70654538d1d9cf3d43bf0e298d913111852
snfry <snoo999@naver.com>
2026-04-10 15:13:42 +0900

    Add Code06_12.java

---
+++ src/main/java/Code06_12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Code06_12 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int total = 0, num1, num2;
+        for (;;) {
+            System.out.print("num1 => ");
+            num1 = s.nextInt();
+            System.out.print("num2 => ");
+            num2 = s.nextInt();
+            System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
+        }
+    }
+}
