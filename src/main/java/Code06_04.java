b8d52535198d5b09c47ce6b090333b1106bcf7c2
snfry <snoo999@naver.com>
2026-04-10 14:15:54 +0900

    Add Code06_04.java

---
+++ src/main/java/Code06_04.java
@@ -0,0 +1,8 @@
+public class Code06_04 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 10; i += 2) {
+            // if (i % 2 == 1) continue;
+            System.out.print(i + "\t");
+        }
+    }
+}
