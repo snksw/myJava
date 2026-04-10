46d1896d6e1bf6cfd58158b080c02225937b0504
snfry <snoo999@naver.com>
2026-04-10 15:13:32 +0900

    Add Code06_09.java

---
+++ src/main/java/Code06_09.java
@@ -0,0 +1,11 @@
+public class Code06_09 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 5; k++) {
+                    System.out.printf("[%d %d %d]\n", i, j, k);
+                }
+            }
+        }
+    }
+}
