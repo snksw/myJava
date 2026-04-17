b55f6b98207badbb032a836ecddf3e83f71b62e2
snfry <snoo999@naver.com>
2026-04-17 15:32:56 +0900

    getter/setter

--- src/main/java/Code07_06.java
+++ src/main/java/Code07_06.java
@@ -3,23 +3,35 @@ class Rabbit {
     private int xPos, yPos;
 
     Rabbit() {
-        shape = "잘 모르는 토끼";
+        this.shape = "잘 모르는 토끼";
     }
 
     Rabbit(String val) {
-        shape = val;
+        this.shape = val;
     }
 
-    void setPosition(int x, int y) {
-        xPos = x; yPos = y;
+    public String getShape() {
+        return this.shape;
     }
 
     public void setShape(String val) {
-        shape = val;
+        this.shape = val;
+    }
+
+    public int getXPos() {
+        return this.xPos;
+    }
+
+    public int getYPos() {
+        return this.yPos;
+    }
+
+    public void setPosition(int x, int y) {
+        this.xPos = x; this.yPos = y;
     }
 
     public String toString() {
-        return "[Rabbit Shape:\"" + shape + "\", Pos: [" + xPos + ", " + yPos + "]]";
+        return "[Rabbit Shape:\"" + this.shape + "\", Pos: [" + this.xPos + ", " + this.yPos + "]]";
     }
 }
 
@@ -27,14 +39,21 @@ public class Code07_06 {
     public static void main(String[] args) {
         Rabbit rabbit0 = new Rabbit();
         System.out.println(rabbit0);
+        System.out.println(rabbit0.getClass());
 
         Rabbit rabbit1 = new Rabbit("원");
         System.out.println(rabbit1);
+        System.out.println(rabbit1.getShape());
 
         Rabbit rabbit2 = new Rabbit("삼각형");
         // rabbit2.shape = "작은 토끼";
         rabbit2.setShape("작은 토끼");
+        // rabbit2.xPos = 40;
+        // rabbit2.yPos = 80;
+        rabbit2.setPosition(40, 80);
         System.out.println(rabbit2);
+        System.out.println(rabbit2.getXPos());
+        System.out.println(rabbit2.getYPos());
 
         Rabbit rabbit3 = new Rabbit("토끼");
         rabbit3.setPosition(10, 20);
