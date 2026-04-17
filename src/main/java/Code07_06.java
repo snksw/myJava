c5abde8b60570dfd4704edf28f84c92f3f08d2a9
snfry <snoo999@naver.com>
2026-04-17 14:57:16 +0900

    Constructor with a parameter

--- src/main/java/Code07_06.java
+++ src/main/java/Code07_06.java
@@ -3,8 +3,8 @@ public class Code07_06 {
         String shape = "default";
         int xPos, yPos;
 
-        Rabbit() {
-            shape = "토끼";
+        Rabbit(String val) {
+            shape = val;
         }
 
         void setPosition(int x, int y) {
@@ -16,14 +16,14 @@ public class Code07_06 {
         }
     }
     public static void main(String[] args) {
-        Rabbit rabbit1 = new Rabbit();
+        Rabbit rabbit1 = new Rabbit("원");
         System.out.println(rabbit1);
 
-        Rabbit rabbit2 = new Rabbit();
+        Rabbit rabbit2 = new Rabbit("삼각형");
         rabbit2.shape = "작은 토끼";
         System.out.println(rabbit2);
 
-        Rabbit rabbit3 = new Rabbit();
+        Rabbit rabbit3 = new Rabbit("토끼");
         rabbit3.setPosition(10, 20);
         System.out.println(rabbit3);
     }
