caf298f22dc77b84795751156ae45f2c265795ea
snfry <snoo999@naver.com>
2026-04-17 15:18:31 +0900

    Privatized

--- src/main/java/Code07_06.java
+++ src/main/java/Code07_06.java
@@ -1,24 +1,29 @@
-public class Code07_06 {
-    static class Rabbit {
-        String shape = "default";
-        int xPos, yPos;
+class Rabbit {
+    private String shape = "default";
+    private int xPos, yPos;
 
-        Rabbit() {
-            shape = "잘 모르는 토끼";
-        }
-
-        Rabbit(String val) {
-            shape = val;
-        }
-
-        void setPosition(int x, int y) {
-            xPos = x; yPos = y;
-        }
-
-        public String toString() {
-            return "[Rabbit Shape:\"" + shape + "\", Pos: [" + xPos + ", " + yPos + "]]";
-        }
+    Rabbit() {
+        shape = "잘 모르는 토끼";
     }
+
+    Rabbit(String val) {
+        shape = val;
+    }
+
+    void setPosition(int x, int y) {
+        xPos = x; yPos = y;
+    }
+
+    public void setShape(String val) {
+        shape = val;
+    }
+
+    public String toString() {
+        return "[Rabbit Shape:\"" + shape + "\", Pos: [" + xPos + ", " + yPos + "]]";
+    }
+}
+
+public class Code07_06 {
     public static void main(String[] args) {
         Rabbit rabbit0 = new Rabbit();
         System.out.println(rabbit0);
@@ -27,7 +32,8 @@ public class Code07_06 {
         System.out.println(rabbit1);
 
         Rabbit rabbit2 = new Rabbit("삼각형");
-        rabbit2.shape = "작은 토끼";
+        // rabbit2.shape = "작은 토끼";
+        rabbit2.setShape("작은 토끼");
         System.out.println(rabbit2);
 
         Rabbit rabbit3 = new Rabbit("토끼");
