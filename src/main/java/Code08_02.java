65877f180c7e9198fc571f90cd06027fbe7cf5b9
snfry <snoo999@naver.com>
2026-05-08 13:52:53 +0900

    Inheritance

---
+++ src/main/java/Code08_02.java
@@ -0,0 +1,30 @@
+public class Code08_02 {
+    public static void main(String[] args) {
+    }
+}
+
+class Rabbit08_02 {
+    String shape;
+    int xPos, yPos;
+
+    void move(int x, int y) {
+        this.xPos = x;
+        this.yPos = y;
+    }
+}
+
+class HouseRabbit08_02 extends Rabbit08_02 {
+    String owner;
+
+    void eatFeed() {
+        System.out.println("집토끼가 사료를 먹습니다.");
+    }
+}
+
+class MountainRabbit08_02 extends Rabbit08_02 {
+    String mountain;
+
+    void eatGrass() {
+        System.out.println("산토끼가 풀을 먹습니다.");
+    }
+}
